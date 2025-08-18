0163c79fa1fdb8bf005494aa3dad9f2aa00719c0
Esteban <jcarranza@unal.edu.co>
2025-08-18 15:04:01 -0500

    fixed sha256 generation

--- src/controllers/wompi/getHashSHA256.js
+++ src/controllers/wompi/getHashSHA256.js
@@ -1,12 +1,16 @@
+import { createHash } from 'crypto';
 
 const getHashSHA256 = async (phrase) => {
 
-    const encondedText = new TextEncoder().encode(phrase);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", encondedText);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+    // const encondedText = new TextEncoder().encode(phrase);
+    // const hashBuffer = await crypto.subtle.digest("SHA-256", encondedText);
+    // const hashArray = Array.from(new Uint8Array(hashBuffer));
+    // const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
 
-    return hashHex;
+    // return hashHex;
+    return createHash('sha256')
+        .update(phrase)
+        .digest('hex');
 };
 
 export default getHashSHA256;
